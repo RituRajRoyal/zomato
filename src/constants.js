20e05b2e1f2b20ebe457923617104856480a9c4a
Ritu Raj Kumar <38011647+RituRajRoyal@users.noreply.github.com>
2021-08-27 12:40:50 +0530

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
-const DEVELOPMENT_URL = "https://glacial-sierra-72676.herokuapp.com";
+const DEVELOPMENT_URL = "https://glacial-sierra-72676.herokuapp.com/";
 const PRODUCTION_URL = "";
 
 exports.API_URL = DEVELOPMENT_URL;
